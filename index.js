1e31d8a882ab9940af00bd468742ef17402f65ca
David <chen7david@me.com>
2020-03-18 15:03:42 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-module.exports = (notify) => (state) => (req, res, next) => {
+const { Notify } = require('notify-io')
+
+module.exports = (schema) => (state) => (req, res, next) => {
     if(!req.tools) req.tools = {}
+    const notify = new Notify(schema)
     notify.stateTo(state)
     req.tools[state] = (code, data, key) => code ? notify.load(code, data, key) : notify
     next()
